c24b920a628b8c7574ce5a57728747ddd165a13a
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-03-06 22:16:54 -0500

    update

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
